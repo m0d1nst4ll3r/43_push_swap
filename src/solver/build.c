3fb98c30a6726215b696007870add8f2880dec02
Raphael Pohlen <rapohlen@f6r13s4.clusters.42paris.fr>
2026-01-13 13:39:27 +0100

    Added duplicate error handling to solver build.c

--- src/solver/build.c
+++ src/solver/build.c
@@ -6,11 +6,26 @@
 /*   By: rapohlen <rapohlen@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/22 22:48:42 by rapohlen          #+#    #+#             */
-/*   Updated: 2025/12/23 09:47:27 by rapohlen         ###   ########.fr       */
+/*   Updated: 2026/01/13 13:38:05 by rapohlen         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "solver.h"
+
+static int	is_duplicate(t_stack *head, int val)
+{
+	t_stack	*cur;
+
+	cur = head;
+	while (1)
+	{
+		if (head->val == val)
+			return (1);
+		head = head->next;
+		if (head == cur)
+			return (0);
+	}
+}
 
 void	build_stack(t_solver *d)
 {
@@ -21,7 +36,8 @@ void	build_stack(t_solver *d)
 	i = 1;
 	while (d->av[i])
 	{
-		if (ft_atoi_strict(d->av[i], &val))
+		if (ft_atoi_strict(d->av[i], &val)
+			|| (d->stacka && is_duplicate(d->stacka, val)))
 			error_out(*d);
 		new = stack_new(val);
 		if (!new)
